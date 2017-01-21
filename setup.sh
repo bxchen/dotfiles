b3c2ad9a1e5c6bae837eef360b64c5fff91b6f2b
bxchen <bxchen@uchicago.edu>
2017-01-20 18:47:20 -0600

    added tmux.conf into dotfiles

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old            # old dotfiles backup directory
-files="zshrc bashrc vimrc vim ssh/config"    # list of files/folders to symlink in homedir
+files="zshrc bashrc vimrc vim ssh/config tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
